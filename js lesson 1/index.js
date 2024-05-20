1e9eb695762b2b88e90c65a1ca498b2a7e2d29da
max yarish <maxyarish2011@gmail.com>
2024-05-20 18:58:07 +0300

    lesson js3

---
+++ js lesson 1/index.js
@@ -0,0 +1,230 @@
+/*console.info(3+4,12,'hi');
+console.log('hi ,console');
+console.error('hi'+ 8);
+console.warn('hi,38');
+//Number число
+console.info(4.56);
+console.log(45);
+//String рядок
+console.log("word");
+console.log("п'ятниця");
+console.log('п\'ятниця');
+console.log('назва книги "мобі діг" ');
+console.log('п\n ятниця');
+console.log('п\t ятниця');
+console.log('п\a ятниця');
+console.log('п\f ятниця');
+console.log(`
+      ▄▄████▄▄▄░░░░░░▄▄██████▄▄
+     ██▓▓▓▓▓▓▒▓▓██░░▓█▓▓▓▓▒░▒▒▓▓██
+   ██▓▓▓▓▓▓▓▓▓▒░▓▓███▓▓▓▓▓▒▒▒░░▒▓▓█
+  ██▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓█▓▓▓▓▓▓▓▓▓▓▒░░▓▓█
+ █▓▓▓▓▓▓▓▓▓▓▓▓▓▓██▓██▓▓▓▓▓▓▓▓▓▒░░▓▓█
+ █▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░▒▓█
+ █▓▓▓▓▓▓▓▓▓▓▓▓████░████▓▓▓▓▓▓▓▓▓▒░░▓██
+█▓▓▓▓▓▓▓▓▓▓▓▓██░░░░░░░██▓▓▓▓▓▓▓▓▓░░▓▓█
+█▓▓▓▓▓▓▓▓▓▓▓▓▓░░█░░░█░░▓▓▓▓▓▓▓▓▓▓▒░▓▓█▒
+█▓▓▓▓▓▓▓▓▓▓▓▓▓░█▒█░█▒█░▓▓▓▓▓▓▓▓▓▓▒▒▓▓█▒
+█▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░░░░░░▓▓▓▓▓▓▓▓▓▓▒▒▓██▒
+█▓▓▓▓▓▓▓▓▓▓▓▓▓▓░▒██░░░▓▓▓▓▓▓▓▓▓▓▓▒▓▓█▓▒
+ █▓▓▓▓▓▓▓▓▓▓▓▓▓▓████▓▓▓▓▓▓▓▓▓▓▓▓▓▒▓▓█▓▒
+ █▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█▓▒
+  █▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█▓▒
+  █▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓███▓▓▓▓▓▓▓▓▓▓█▓▒
+   █▓▓▓▓▓▓▓▓▓██▓▓▓▓█████▓▓▓▓▓▓▓▓▓▓█▓▒
+    █▓▓▓▓▓▓▓▓██████████▓▓▓▓▓▓▓▓▓▓██▓▒
+     █▓▓▓▓▓▓▓▓██▒▒▓▒▒█▓▓▓▓▓▓▓▓▓▓██▓▒
+      █▓▓▓▓▓▓▓▓██▒▒▒█▓▓▓▓▓▓▓▓▓▓██▓▒
+       ██▓▓▓▓▓▓▓▓███▓▓▓▓▓▓▓▓▓▓██▓▒
+        ██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██▓▒
+          █▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█▓▒
+           ██▓▓▓▓▓▓▓▓▓▓▓▓██▓▒
+            ██▓▓▓▓▓▓▓▓▓█▓▒
+              ██▓▓▓▓▓██▓▒                    
+                █▓▓██▓▒ 
+                  █▓▒
+`);
+//Boolean логічний
+console.log(true);
+console.log(false);
+//null нічого
+console.log(null);
+//undefined незнаємо
+console.log(undefined);
+document.write('9+9\f'); 
+document.write(9+9);
+
+
+
+
+
+let my_name = 'max';
+console.log('firstname');
+let year_a = 1978;
+console.log(year_a);
+
+
+
+let my_favoriteColor ='red';
+console.log(my_name, my_favoriteColor);
+
+
+let isStudent=false
+
+let age;
+console.log(age);
+age = 18;
+ console.log(age);
+ 
+ 
+let myAnimal ='cat';
+console.log(myAnimal);
+
+const myWakeuptime ='12:00';
+console.log(myWakeuptime);
+
+
+const alldayweek='7 days in week' ;
+console.log(alldayweek);
+
+const youArejoob='no';
+console.log(youArejoob);
+
+const myGrowh=1.56;
+console.log(myGrowh);
+let 
+youareRobot=false;
+
+*/
+
+/*
+const currentYear = new Date().getFullYear();
+
+const userName=prompt('enter your name');
+alert(userName + ',' + 'thanks');
+const userBirthage=prompt('what your year of birth');
+alert(userName +' '+ userBirthage +' '+ 'thanks');
+
+const userAge = currentYear - userBirthage;
+alert(userName + ',' +'your age'+userAge+'?');
+const userFutureage=userAge+10;
+alert('через 10 років вам буде:' +userFutureage+'?')
+*/
+
+/*
+const Number1 = prompt('enter first number', 5);
+const Number2 = prompt('enter secont number', 3);
+console.log('Number1 =', Number1, typeof Number1);
+console.log('Number1 =', Number2, typeof Number2);
+
+const Summa1 = Number1 + Number2;
+alert('Summa1 =' +Summa1);
+
+const Summa2 = Number1 + Number2;
+alert('Summa2 ='+Summa2);
+
+const Summa3 = Number(Number1) + Number(Number2);
+alert('Summa3 ='+Summa3);
+
+const Substruct = Number(Number1) - Number(Number2);
+alert(Substruct);
+
+*/
+/*
+const carName1=prompt('введіть назву машини');
+const priceCar1=prompt('введіть ціну машини');
+const carname2=prompt('введіть назву машини');
+const priceCar2=prompt('введіть ціну машини');
+const symmaCars=Number(priceCar2) + Number(priceCar2);
+alert('ви бажаєте купили: \n' + carName1 + ' - ' + priceCar1 +'\n' +carname2 +' - ' +priceCar2 )
+alert('до оплати:'+ symmaCars) 
+*/
+/*
+const product1=prompt('введіть назву продукта');
+const price1=prompt('введіть його ціну в гривнях');
+const product2=prompt('введіть назву продукта');
+const price2=prompt('введіть його ціну в гривнях');
+const Summa1=Number(price1)+ Number(price2);
+alert('Ви бажаєте купили:\n' + product1 + ' - ' + price1+ '\n' + product2 + ' - ' + price2); 
+alert('До оплати: ' + Summa1+' гривень');
+*/
+/*
+const userName=prompt('enter your name');
+const isUserName=confirm('this is your name?'+userName+'?');
+if (isUserName){
+  alert('hi, '+userName);
+} else {
+  alert('be more careful next time');
+}   
+ 
+  */
+/*
+const yourPrezent=prompt('enter you presernt which you want:');
+const isYourPrezent=confirm('you want this prezent?');
+if (isYourPrezent){
+  alert('okay waiting your prezent');
+} 
+else {
+  alert('present cancel');
+}
+ */
+
+/*
+let amoult = 0
+
+if (confirm('is the sun a star?')) {
+amoult= amoult + 1
+}
+alert('ваш результат:'+amoult+'/5 бала');
+if (!confirm('чи є місяць планетою')){
+  amoult=amoult + 1 ;
+  alert('ваш результат:'+amoult+'/5 бала');
+}
+alert('ваш результат:'+amoult+'/5 бала');
+if(!confirm('чи україна більша за всю європу?')){
+  amoult=amoult+1;
+}
+ alert('ваш результат:'+amoult+'/5 бала');
+if(confirm('чи можна зробити вогонь за допомогою креміню')){
+  amoult=amoult+1;
+}
+ alert('ваш результат: '+amoult+'/5 бала');
+if(!confirm('чи HTML-5 і CSS-3 вважаються мовами програмування' )){
+amoult=amoult+1
+}
+alert('ваш результат: '+amoult+'/5 бала');
+*/
+/*
+let amoult = 0
+
+if (confirm('is the sun a star?')) {
+amoult++;
+}
+alert('ваш результат:'+amoult+'/4 бала');
+if (!confirm('чи є місяць планетою')){
+  amoult++;
+}
+alert('ваш результат:'+amoult+'/4 бала');
+if(!confirm('чи україна більша за всю європу?')){
+  amoult++;
+}
+alert('ваш результат:'+amoult+'/4 бала');
+if(confirm('чи можна зробити вогонь за допомогою креміню')){
+  amoult=amoult+1;
+}
+alert('ваш результат: '+amoult+'/4 бала');
+alert('ваша середня оцінка '+amoult/2+'бала');
+*/
+
+if(confirm('чи буде конвертація')){
+  const usdValut= prompt('введіть сумму долара');
+  const usdCours=prompt('ввудіть курс в доларах')
+  const result = usdValut * usdCours;
+  alert('ви отримаєте' + result +'гривень')
+}
+else{
+  const uahValut= prompt('введіть сумму гривнях');
+  const uahCours=prompt('ввудіть курс в гривнях')
+  const result = uahValut / uahCours;
+  alert('ви отримаєте ' + result +'доларів')
+}
