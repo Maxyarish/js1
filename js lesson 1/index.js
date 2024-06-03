0051cc7a697c16b0bd579fd5884df7f374d59e2c
max yarish <maxyarish2011@gmail.com>
2024-06-03 22:41:30 +0300

    practicJs2

--- js lesson 1/index.js
+++ js lesson 1/index.js
@@ -95,9 +95,7 @@ console.log(myGrowh);
 let 
 youareRobot=false;
 
-*/
 
-/*
 const currentYear = new Date().getFullYear();
 
 const userName=prompt('enter your name');
@@ -109,9 +107,7 @@ const userAge = currentYear - userBirthage;
 alert(userName + ',' +'your age'+userAge+'?');
 const userFutureage=userAge+10;
 alert('через 10 років вам буде:' +userFutureage+'?')
-*/
 
-/*
 const Number1 = prompt('enter first number', 5);
 const Number2 = prompt('enter secont number', 3);
 console.log('Number1 =', Number1, typeof Number1);
@@ -129,102 +125,438 @@ alert('Summa3 ='+Summa3);
 const Substruct = Number(Number1) - Number(Number2);
 alert(Substruct);
 
-*/
-/*
+
 const carName1=prompt('введіть назву машини');
 const priceCar1=prompt('введіть ціну машини');
-const carname2=prompt('введіть назву машини');
-const priceCar2=prompt('введіть ціну машини');
-const symmaCars=Number(priceCar2) + Number(priceCar2);
-alert('ви бажаєте купили: \n' + carName1 + ' - ' + priceCar1 +'\n' +carname2 +' - ' +priceCar2 )
-alert('до оплати:'+ symmaCars) 
+const carname2=prompt('введіть назву машини
+}
 */
 /*
-const product1=prompt('введіть назву продукта');
-const price1=prompt('введіть його ціну в гривнях');
-const product2=prompt('введіть назву продукта');
-const price2=prompt('введіть його ціну в гривнях');
-const Summa1=Number(price1)+ Number(price2);
-alert('Ви бажаєте купили:\n' + product1 + ' - ' + price1+ '\n' + product2 + ' - ' + price2); 
-alert('До оплати: ' + Summa1+' гривень');
-*/
-/*
-const userName=prompt('enter your name');
-const isUserName=confirm('this is your name?'+userName+'?');
-if (isUserName){
-  alert('hi, '+userName);
-} else {
-  alert('be more careful next time');
-}   
- 
-  */
-/*
-const yourPrezent=prompt('enter you presernt which you want:');
-const isYourPrezent=confirm('you want this prezent?');
-if (isYourPrezent){
-  alert('okay waiting your prezent');
-} 
-else {
-  alert('present cancel');
-}
- */
+const userInput=prompt('enter number');
+console.log(userInput);
+const userNumber=Number(userInput);
+console.log(userNumber)
 
-/*
-let amoult = 0
-
-if (confirm('is the sun a star?')) {
-amoult= amoult + 1
+const error1 =userInput===null;
+const error2 =userInput ==='';
+const error3 =Number.isNaN(userNumber);
+if(error1 || error2 ||error3){
+  alert('ERROR\n '+ +userInput+'- is not number!');
 }
-alert('ваш результат:'+amoult+'/5 бала');
-if (!confirm('чи є місяць планетою')){
-  amoult=amoult + 1 ;
-  alert('ваш результат:'+amoult+'/5 бала');
-}
-alert('ваш результат:'+amoult+'/5 бала');
-if(!confirm('чи україна більша за всю європу?')){
-  amoult=amoult+1;
-}
- alert('ваш результат:'+amoult+'/5 бала');
-if(confirm('чи можна зробити вогонь за допомогою креміню')){
-  amoult=amoult+1;
-}
- alert('ваш результат: '+amoult+'/5 бала');
-if(!confirm('чи HTML-5 і CSS-3 вважаються мовами програмування' )){
-amoult=amoult+1
-}
-alert('ваш результат: '+amoult+'/5 бала');
-*/
-/*
-let amoult = 0
-
-if (confirm('is the sun a star?')) {
-amoult++;
-}
-alert('ваш результат:'+amoult+'/4 бала');
-if (!confirm('чи є місяць планетою')){
-  amoult++;
-}
-alert('ваш результат:'+amoult+'/4 бала');
-if(!confirm('чи україна більша за всю європу?')){
-  amoult++;
-}
-alert('ваш результат:'+amoult+'/4 бала');
-if(confirm('чи можна зробити вогонь за допомогою креміню')){
-  amoult=amoult+1;
-}
-alert('ваш результат: '+amoult+'/4 бала');
-alert('ваша середня оцінка '+amoult/2+'бала');
-*/
-
-if(confirm('чи буде конвертація')){
-  const usdValut= prompt('введіть сумму долара');
-  const usdCours=prompt('ввудіть курс в доларах')
-  const result = usdValut * usdCours;
-  alert('ви отримаєте' + result +'гривень')
+else if(userNumber>=0 && userNumber <=23){
+  alert('thanks');
 }
 else{
-  const uahValut= prompt('введіть сумму гривнях');
-  const uahCours=prompt('ввудіть курс в гривнях')
-  const result = uahValut / uahCours;
-  alert('ви отримаєте ' + result +'доларів')
+  alert('ERROR\n '+ +userInput+'- is not hour!');
 }
+*/
+/*
+const enterNum1=prompt('enter 1 number');
+const enterNum2=prompt('enter 2 number');
+
+const number1 =Number(enterNum1);
+const number2 =Number(enterNum2);
+
+if(number1 && number2 >0){
+alert('positive');
+}else if(number1 && number2 <0){
+alert('negative');
+}else{
+alert('zero');
+}
+
+const summa= prompt('enter summa');
+const minSumma=1000;
+let delivery;
+if (Number(summa)>minSumma) {
+  delivery=0;
+  alert('безплатна доставка')
+} else {
+  delivery=70;
+  alert('досплати: '+(Number(summa)+delivery)+'гривень')
+}
+
+const summa= prompt('enter summa');
+const minSumma=1000;
+const delivery=(Number(summa)>=minSumma)? 0 : 50 ;
+if (Number(summa)>minSumma) {
+  
+  alert('безплатна доставка з вас: '+summa + 'гривень')
+} else {
+  alert('досплати: '+(Number(summa)+delivery)+' гривень')
+}
+
+
+const number1=prompt('enter number');
+
+const wordEven0dd=Number(number1)/2 ===0 ? 'even':'odd';
+alert(number1+' ' + wordEven0dd);
+
+
+const priceTicket=100;
+const userAgeInput=prompt('enter your age');
+const userAge=Number(userAgeInput);
+const sale=(userAge<18 || userAge >65 )? 10:0;
+const saleCheck=priceTicket - (priceTicket*sale/100);
+alert('до сплати: ' + saleCheck);
+ 
+
+const number1=prompt('enter number');
+const number2 = (number1 >= 0) ? number1 : -number1;
+alert ( 'Модуль числа: ' + number2);
+
+const enterInfo1 = prompt('Введіть першу довжину суміжних сторін прямокутника');
+const enterInfo2 = prompt('Введіть другу довжину суміжних сторін прямокутника');
+
+const kvadrat = 'квадрат';
+const pryamokutnyk = 'прямокутник';
+
+let result = (enterInfo1 === enterInfo2) ? kvadrat : pryamokutnyk;
+alert(result);
+
+const message =`введіть мову,якою хочеш спілкуватися:
+1 українська - ua
+2 англійська - en
+3 польска - pl
+4 іспанська - es
+5 французька - fr
+`;
+                         
+const lang =prompt(message);
+
+switch (lang) {
+  case 'ua':
+    case '1':
+      case'українська':
+    alert('привіт');
+    break;
+case 'en': 
+case '2':
+case 'англійська':
+alert('hi');
+break;
+case 'pl': 
+case'3':
+case'польска':
+alert('cheść');
+break;
+case 'es': 
+case'4':
+case 'іспанська':
+alert('hola');
+break;
+case 'fr': 
+case '5':
+case'французька':
+alert('bonjour');
+break;
+  default:
+    alert('мова не підтримується');
+    break;
+}
+if (lang ==='ua' || lang ==='1' || lang ==='українська'){
+  alert('привіт');
+}else if(lang ==='en'|| lang ==='2'||lang==='англійська'){
+  alert('hi');
+}else if(lang ==='pl'|| lang ==='3'|| lang==='польска'){
+alert('cześć')
+}else if(lang ==='es'|| lang ==='4'|| lang ==='іспанська'){
+  alert('hola')                                                               
+}else if(lang ==='fr'|| lang ==='5'|| lang ==='французька'){
+  alert('bonjour')
+}else{
+  alert('мова не підтримується');
+}
+console.log(lang);
+ 
+
+switch(confirm('you want exit in this site?')){
+  case true:
+    alert('good bay');
+    break;
+    case false:
+      alert('thank you')
+      break;
+}
+confirm('you want exit in this site?')
+? alert('good bay')
+: alert('thank you')
+
+
+const  message =`введіть день(число) тижня
+1 
+2 
+3 
+4 
+5
+6
+7
+`;
+
+const enterWeek=prompt(message);
+switch (enterWeek) {
+  case '1':
+    alert('понеділок');
+    break;
+    case '2':
+      alert('вівторок');
+      break;
+      case '3':
+        alert('середа');
+        break;
+        case '4':
+         alert('четверг') ;
+         break;
+         case '5':
+          alert('п\'ятниця');
+          break;
+          case '6':
+            alert('суботта');
+            break;
+            case '7':
+              alert('неділя')
+  default:
+    alert('днів у тижні є тільки 7')
+    break;
+}
+
+
+const  message =`введіть день(число) тижня
+1 
+2 
+3 
+4 
+5
+6
+7
+`;
+const enterWeek=prompt(message);
+switch (enterWeek) {
+  case '1':
+    case '2':
+      case '3':
+        case '4':
+    case '5':
+    alert('work');
+    break;
+          case '6':
+            case '7':
+            alert('weekend');
+            break;
+  default:
+    alert('днів у тижні є тільки 7')
+    break;
+}
+
+
+const userInput1 =prompt('enter first Number');
+const userInput2 =prompt('enter first Number');
+const userNum1=Number(userInput1);
+const userNum2=Number(userInput2);
+const userNumD1=(userInput1);
+const userNumD2=(userInput2);
+const operator=prompt(`enter operator:
++
+-
+*
+%
+/
+=
+`);
+switch(operator){
+case '+':
+alert(userNum1+userNum2);
+break;
+case '-':
+alert(userNum1-userNum2);
+break;
+case '*':
+alert(userNum1*userNum2);
+break;
+case'%':
+alert(userNum1%userNum2);
+break;
+case'/':
+alert(userNum1/userNum2);
+break;
+case '=':
+  alert(userNumD1+userNumD2)
+  break;
+  default:
+    alert('operator not exits')
+    break;
+}
+
+
+console.group('task 1');
+         console.log('task 1');
+console.groupEnd();
+
+
+const userAge=prompt('enter your age');
+const endUserAge=Number(userAge) >18 ?'ви повнолітній':'ви не повнолітній';
+alert(endUserAge)
+
+
+console.group('task 2');
+         console.log('task 2');
+console.groupEnd();
+
+const userAge=prompt('enter your age');
+const userAgeEnd=Number(userAge);
+if(userAge <0){
+  alert('вік не може бути менше 0');
+}
+else if (userAgeEnd <12) {
+  alert('дитина');
+}else if(userAgeEnd <18) {
+  alert('підліток');
+}else if(userAgeEnd <65){
+  alert('дорослий');
+}else{
+  alert('старий');
+}
+
+
+console.group('task 3');
+         console.log('task 3');
+console.groupEnd();
+const enterUser=prompt('enter your time');
+const enterUserInfo=Number(enterUser);
+if (enterUserInfo>=0 && enterUserInfo<5 || enterUserInfo>=22 && enterUserInfo<24){
+  alert('доброї ночі');
+}else if (enterUserInfo <11){
+  alert('доброго ранку');
+}else if(enterUserInfo <18){
+  alert('доброго дня');
+}else if(enterUserInfo <22){
+  alert('доброго вечора');
+}else{
+  alert('не буває такої години');
+}
+
+console.group('task 4');
+         console.log('task 4');
+console.groupEnd();
+const enterTime1=prompt('enter time')
+const allTime=enterTime1<9 && enterTime1<17 || enterTime1>=18?'не працює' :'працює'
+alert(allTime);
+
+
+console.group('task 5');
+console.log('task 5');
+console.groupEnd();
+const userInput=prompt('введіть номер від 1 до 12')
+switch (userInput) {
+  case '1':
+    alert('січень');
+    break;
+    case '2':
+      alert('лютий');
+      break
+      case '3':
+        alert('березень');
+        break
+        case '4':
+          alert('квітень');
+          break
+          case '5':
+            alert('травень');
+            break
+            case'6':
+            alert('червень');
+            break
+            case'7':
+            alert('липень');
+            break
+            case'8':
+            alert('серпень');
+            break
+            case '9':
+              alert('вересень');
+              break
+              case '10':
+                alert('жовтень');
+                break
+                case '11':
+                  alert('листопад');
+                  break
+                  case '12':
+                    alert('грудень');
+                    break
+  default:
+    alert('введіть від 1 до 12');
+    break;
+}
+
+console.group('task 6');
+console.log('task 6');
+console.groupEnd();
+const userInput=prompt('введіть номер від 1 до 12')
+switch (userInput) {
+  case '1':
+    case '2':
+      case '12':
+      alert('зима');
+      break
+      case '3':
+        case '4':
+          case '5':
+            alert('весна')
+            break
+            case'6':
+            case'7':
+            case'8':
+            alert('літо');
+            break
+            case '9':
+              case '10':
+                case '11':
+                  alert('осінь');
+                  break
+  default:
+    alert('введіть від 1 до 12');
+    break;
+}
+
+
+
+console.group('task 7');
+console.log('task 7');
+console.groupEnd();
+const enterNum1 = prompt('введіть число:');
+const enterNum2 = prompt('введіть число:');
+const enterNum3 = prompt('введіть число:');
+
+if (enterNum1 || enterNum2 || enterNum3) {
+  alert('Будь ласка, введіть дійсні числа для всіх трьох сторін');
+} else {
+  if (enterNum1 === enterNum2 && enterNum2 === enterNum3) {
+    alert('Трикутник є рівносторонній');
+  } else if (enterNum1 === enterNum2 || enterNum1 === enterNum3 || enterNum2 === enterNum3) {
+    alert('Трикутник є рівнобедрений');
+  } else {
+    alert('трикутник є різносторонній');
+  }
+}
+
+console.group('task 8');
+console.log('task 8');
+console.groupEnd();
+const enterUserBall=prompt('enter your ball');
+const alfavitBall=Number(enterUserBall);
+if(alfavitBall <=10){
+  alert('F-')
+}
+else if (alfavitBall <=20) {
+  alert('F');
+} else if(alfavitBall <=40) {
+  alert('D');
+}else if(alfavitBall <=60){
+  alert('C');
+}else if(alfavitBall <=80){
+  alert('B');
+}else{
+  alert('A');
+}
+*/
